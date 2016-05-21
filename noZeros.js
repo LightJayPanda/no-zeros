0a17ac38b33443332ee8b169437c9f56756b6bf3
LightJayPanda <lightjaypanda@gmail.com>
2016-05-21 22:02:11 +0300

    first commit; tests return undefined

---
+++ noZeros.js
@@ -0,0 +1,12 @@
+function noBoringZeros(n) {
+  var arr = n.toString;
+  arr.split;
+  for ( var i = (arr.length - 1); i >= 0; i--) {
+    if ( arr[i] == 0) {
+      arr.pop();
+    };
+  n = arr.join;
+  n = parseInt(n);
+  }
+  console.log(n);
+}
